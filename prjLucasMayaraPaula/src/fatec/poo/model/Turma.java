49a3059c6c724163f80d42caf1c70fa184ffb061
lschiming <lucas.schiming@gmail.com>
2018-05-23 19:50:49 -0300

    Renomeado o projeto (faltou o POO)

---
+++ prjLucasMayaraPaula/src/fatec/poo/model/Turma.java
@@ -0,0 +1,15 @@
+
+package fatec.poo.model;
+
+public class Turma {
+    private String siglaTurma, descricao, dataInicio, dataTermino, periodo, observacoes;
+    private int qtdVagas;
+    private Curso curso;
+
+    public Turma(String siglaTurma, String descricao) {
+        this.siglaTurma = siglaTurma;
+        this.descricao = descricao;
+    }
+    
+    
+}

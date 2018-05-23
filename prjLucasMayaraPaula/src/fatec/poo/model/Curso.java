0fa98ef6744631ca93f43544994f3698580104a3
lschiming <lucas.schiming@gmail.com>
2018-05-22 21:53:27 -0300

    Commit classe Curso

---
+++ prjLucasMayaraPaula/src/fatec/poo/model/Curso.java
@@ -0,0 +1,15 @@
+
+package fatec.poo.model;
+
+public class Curso {
+    private String sigla, nome, dataVigencia, programa;
+    private int cargaHoraria;
+    private double valor, valorHoraInstrutor;
+
+    public Curso(String sigla, String nome) {
+        this.sigla = sigla;
+        this.nome = nome;
+    }
+    
+    
+}

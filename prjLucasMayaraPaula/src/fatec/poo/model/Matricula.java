478cc9aecfbefe87f2ba7568610d358a589e052a
Paula <paulasartori6@gmail.com>
2018-05-23 23:17:49 -0300

    classe Matricula

---
+++ prjLucasMayaraPaula/src/fatec/poo/model/Matricula.java
@@ -0,0 +1,24 @@
+package fatec.poo.model;
+
+/**
+ *
+ * @author Paula Sartori
+ */
+public class Matricula {
+    private String data;
+    private int qtdeFaltas;
+    private double nota;
+
+    public Matricula(String data) {
+        this.data = data;
+    }
+
+    public void setQtdeFaltas(int qtdeFaltas) {
+        this.qtdeFaltas = qtdeFaltas;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    //emitirCarne como assim nao eh pra implementar???;
+}

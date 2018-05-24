f6eb392235022a65c9508f725f79a2219d6c578b
Paula <paulasartori6@gmail.com>
2018-05-24 16:34:44 -0300

    classe a vista

---
+++ prjLucasMayaraPaula/src/fatec/poo/model/AVista.java
@@ -0,0 +1,29 @@
+package fatec.poo.model;
+
+/**
+ *
+ * @author Paula Sartori
+ */
+public class AVista {
+    private double valor;
+    private int agencia, nCheque;
+    private String preData;
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public void setnCheque(int nCheque) {
+        this.nCheque = nCheque;
+    }
+
+    public void setPreData(String preData) {
+        this.preData = preData;
+    }
+    
+    
+}

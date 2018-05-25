e8543ad47c72008748ea2a4dd344029b11b27ae5
Paula <paulasartori6@gmail.com>
2018-05-24 21:13:27 -0300

    metodos classe a prazo

--- prjLucasMayaraPaula/src/fatec/poo/model/APrazo.java
+++ prjLucasMayaraPaula/src/fatec/poo/model/APrazo.java
@@ -2,6 +2,26 @@
 package fatec.poo.model;
 
 public class APrazo {
-    private int;
+    private int qtdeMensalidade;
+    private double valor, taxaJuros;
+    private String dataVencimento;
+
+    public void setQtdeMensalidade(int qtdeMensalidade) {
+        this.qtdeMensalidade = qtdeMensalidade;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setTaxaJuros(double taxaJuros) {
+        this.taxaJuros = taxaJuros;
+    }
+
+    public void setDataVencimento(String dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+    
+    
     
 }

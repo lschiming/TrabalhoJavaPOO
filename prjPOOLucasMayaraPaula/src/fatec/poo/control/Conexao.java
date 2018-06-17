8a68aed54278dc441991058fe833d8410e17d22b
lschiming <lucas.schiming@gmail.com>
2018-06-17 20:20:56 -0300

    conexao criada

--- prjPOOLucasMayaraPaula/src/fatec/poo/control/Conexao.java
+++ prjPOOLucasMayaraPaula/src/fatec/poo/control/Conexao.java
@@ -1,10 +1,55 @@
 
 package fatec.poo.control;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Lucas
  */
 public class Conexao {
+    private String connectionString, driver, usuario, senha;
+    private Connection conn=null;
     
+    public Conexao(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha   = senha;
+    }
+
+    public void setConnectionString(String connectionString) {
+        this.connectionString = connectionString;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+    
+    public Connection conectar() {
+        
+	   if (conn == null){
+	      try {
+                 Class.forName(driver);
+              
+                 conn = DriverManager.getConnection(connectionString, usuario, senha);
+	         System.out.println("Conexao OK");
+              } catch (Exception ex) {
+                  System.out.println("Falha na Conexao");
+                  System.out.println(ex.toString() + ex.getMessage());
+              }
+	    }
+        return conn;
+    }
+    
+    public void fecharConexao(){
+        if (conn != null){
+	   try {
+                  conn.close();
+           } catch (SQLException ex) {
+                   System.out.println(ex.toString());
+           }
+        }
+    }
+
 }
